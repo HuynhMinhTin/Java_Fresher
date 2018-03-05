6fa5d9e17ca7a3ac010b7805ece000572e363434
nguyethanh <nguyethanh@github.com>
2018-03-05 08:34:06 +0700

    repair merge home page

--- pages/js/customize-search.js
+++ pages/js/customize-search.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 function search(){
 	if(document.getElementById('spoiler').style.display=='none') {document.getElementById('spoiler') .style.display='';
 	} else{
 		document.getElementById('spoiler') .style.display='none';
 	}
-=======
-function search(){
-	if(document.getElementById('spoiler').style.display=='none') {document.getElementById('spoiler') .style.display='';
-	} else{
-		document.getElementById('spoiler') .style.display='none';
-	}
->>>>>>> 3399b4389add1d25bcfbab10a1ddaef80e602f26
 }
--- pages/js/image-file.js
+++ pages/js/image-file.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 jQuery(document).ready(function (){
 	jQuery(".file").attr("src", "../image/file.png");	
-=======
-jQuery(document).ready(function (){
-	jQuery(".file").attr("src", "../image/file.png");	
->>>>>>> 3399b4389add1d25bcfbab10a1ddaef80e602f26
 });
